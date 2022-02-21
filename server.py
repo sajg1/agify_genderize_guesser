c2000f1b055fefc967a1bd5d80f9c4b7cd73b9a7
Steve Gossip <stephengossip@gmail.com>
2022-02-21 21:55:46 +0000

    able to call the agify api and access the age data for the input name. The age is then displayed on the guess_name route.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -9,7 +10,10 @@ def home():
 
 @app.route('/guess_name/<some_name>')
 def guess_name(some_name):
-    return render_template('guess_name.html', name=some_name)
+    age_response = requests.get(url=f'https://api.agify.io?name={some_name}')
+    guessed_age = age_response.json()['age']
+    return render_template('guess_name.html', name=some_name, age=guessed_age)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
