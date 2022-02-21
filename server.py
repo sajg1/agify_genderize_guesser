d51dada0ecb4d06e196428fcf785a871b01f5060
Steve Gossip <stephengossip@gmail.com>
2022-02-21 22:08:02 +0000

    small tidy up

--- server.py
+++ server.py
@@ -8,10 +8,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/guess_name/<some_name>')
+@app.route('/guess/<some_name>')
 def guess_name(some_name):
-    age_response = requests.get(url=f'https://api.agify.io?name={some_name.title()}')
-    gender_response = requests.get(url=f'https://api.genderize.io?name={some_name.title()}')
+    age_response = requests.get(url=f'https://api.agify.io?name={some_name}')
+    gender_response = requests.get(url=f'https://api.genderize.io?name={some_name}')
     guessed_age = age_response.json()['age']
     guessed_gender = gender_response.json()['gender']
     return render_template('guess_name.html', name=some_name, age=guessed_age, gender=guessed_gender)
